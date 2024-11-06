01fbcb2a81805547362ec4a7b7e55e67600a421b
Kelvin-Regino <kelvinregino504@gmail.com>
2024-11-06 14:22:03 -0600

    Add files via upload

---
+++ Cola_Especial.py
@@ -0,0 +1,16 @@
+class   Cola_Especial():
+    
+    cola_principal = [None]*10
+    
+def __init__(self,cola_principal):
+        cola_principal = [None]*10
+        
+def agregar(self):
+    
+    while True:
+        if Cola_Especial.cola_principal[10]=
+        elemento = int(input("Ingrese un elemto: "))
+        Cola_Especial.cola_principal.append(elemento)
+                
+        
+        break
